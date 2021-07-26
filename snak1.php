b0e72225f7b3744392fc5d05e9f963e6fc94bfde
emmanuelparisi <emmanuel.p2606@gmail.com>
2021-07-26 15:53:44 +0200

    add primo snak1

--- snak1.php
+++ snak1.php
@@ -1,7 +1,29 @@
 <!-- Snack 1
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
-<?php echo 'ciao'?>
+<?php 
+$match1giornata = [
+    [
+        'squadraCasa' => 'Parma',
+        'squadraOspite' =>'Bologna',
+        'puntiSquadraCasa' => rand(50,100),
+        'puntiSquadraOspite' => rand(50,100)
+    ],
+    [
+        'squadraCasa' => 'Milano',
+        'squadraOspite' =>'Genova',
+        'puntiSquadraCasa' => rand(50,100),
+        'puntiSquadraOspite' => rand(50,100)
+    ],
+    [
+        'squadraCasa' => 'Perugia',
+        'squadraOspite' =>'Roma',
+        'puntiSquadraCasa' => rand(50,100),
+        'puntiSquadraOspite' => rand(50,100)
+    ]
+];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,6 +33,18 @@ Olimpia Milano - Cantù | 55-60 -->
     <title>snack-1</title>
 </head>
 <body>
+    <h2>Match prima giornata basket</h2>
+    <ul>
+        <?php for($i =0; $i<count($match1giornata); $i++) { ?>
+            <li>
+                <?php 
+                echo 'Olimpia ' . $match1giornata[$i]['squadraCasa'] . ' - ' . $match1giornata[$i]['squadraOspite'] . ' | ' . $match1giornata[$i]['puntiSquadraCasa'] . '-' . $match1giornata[$i]['puntiSquadraOspite']
+                ?>
+            </li>
+            
+        <?php } ?>
+        
+    </ul>
     
 </body>
 </html>
